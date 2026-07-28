343fa12560152b10684bae71ee942e9dc81f6610
caochenxu <cao.chenxu.t@gmail.com>
2026-07-28 13:26:43 +0800

    fix: satisfy frontend license checks

--- shell/app/modules/runtime/pages/overview/components/progressive-release.tsx
+++ shell/app/modules/runtime/pages/overview/components/progressive-release.tsx
@@ -1,5 +1,15 @@
-// Copyright (c) 2026 MosaicPlane Authors.
-// Licensed under the Apache License, Version 2.0.
+// Copyright (c) 2021 Terminus, Inc.
+//
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import React from 'react';
 import { Button, Drawer, Form, InputNumber, message, Modal, Popconfirm, Space, Table, Tag, Tooltip } from 'antd';
